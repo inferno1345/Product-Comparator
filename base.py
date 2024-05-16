7ed89a4e7f386b4a6dcb22605bc78606ca8d8469
Tom <tomrajeev333@gmail.com>
2024-05-16 23:14:26 +0530

    complete price comparator

--- base.py
+++ base.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from numpy import require
+from pyparsing import stringStart
 import requests
 import time
 import atexit
@@ -18,7 +20,8 @@ app = Flask(__name__)
 
 client = OpenAI(api_key = 'sk-proj-P1Geb3FAub2ams0wnB11T3BlbkFJoKKaUmjYdXrlTHv2HzNr')
 
-product1=""
+product1 = ""
+product2 = ""
 
 def get_completion(prompt): 
     messages=[
@@ -38,29 +41,35 @@ def get_completion(prompt):
     return query.choices[0].message.content
     
 def soup(url):
+    string_values = []
+    final_price = []
     if url:
         req = requests.get(url)
         content = BeautifulSoup(req.content, 'html.parser')
-        string_values = []
         
         if 'ebay' in url:
             spec_divs = content.find_all('div', class_='ux-layout-section-evo ux-layout-section--features')
+            price = content.find_all('span', class_='ux-textspans')
         elif 'snapdeal' in url:
             spec_divs = content.find_all('div',class_='tab-container')
+            price = content.find_all('span',class_='payBlkBig')
         else:
             print("Invalid URL:", url)
-            string_values = []
-            return string_values
+            return string_values, final_price
         
         for spec_div in spec_divs:
-            strings = spec_div.stripped_strings
-            string_values.extend(strings)   
-        return string_values
+            text = spec_div.text.strip()
+            string_values.append(text)
+             
+        for div in price:
+            svar = div.text.strip()
+            final_price.append(svar)
+            
+        return string_values, final_price
     
     else:
         print("Invalid URL:", url)
-        string_values = []
-        return string_values
+        return string_values, final_price
     
 path = '/Users/Tom/Documents/Programs/Product Comparator/Product-Comparator/chromedriver-win64/chromedriver.exe'
 service = Service(executable_path=path)
@@ -105,14 +114,14 @@ def selenium_app(url):
         string_values = []
         
     return string_values, final_price.text
-    
+
 @app.route('/price_comp', methods=['POST'])
 def price():
     global driver
     url = product1
     driver.get(url)
     title = product_title = ""
-    url_now1 = url_now2 = price1 = price2 = ""
+    url_now1 = url_now2 = price_now1 = price_now2 = ""
     if 'flipkart' in url:
         title = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="VU-ZEz"]')))
         for titles in title:
@@ -124,39 +133,74 @@ def price():
         input_element.send_keys(product_title + Keys.ENTER)
         url_now1 = driver.current_url
         price = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="a-price-whole"]')))
-        price1 = price[1].text
+        price_now1 = price[1].text
         
-        driver.get("https://www.jiomart.com")
-        input_element = driver.find_element(By.XPATH,'//*[@class="aa-Input search_input"]') 
+    elif 'amazon' in url:
+        title = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="a-size-large product-title-word-break"]')))
+        for titles in title:
+            product_title = titles.text
+            
+        driver.get("https://www.flipkart.com")
+        input_element = driver.find_element(By.XPATH, '//*[@class="Pke_EE"]')
+        input_element.send_keys(product_title + Keys.ENTER)
+        url_now1 = driver.current_url
+        price = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="Nx9bqj _4b5DiR"]')))
+        price_now1 = price[2].text
+    
+    url = product2
+    price = []   
+    title = product_title = ""
+    driver.get(product2)
+    if 'flipkart' in url:
+        title = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="VU-ZEz"]')))
+        for titles in title:
+            product_title = titles.text 
+   
+        driver.get("https://www.amazon.in")
+        input_element = driver.find_element(By.XPATH,'//*[@id="twotabsearchtextbox"]') 
+        time.sleep(1)
         input_element.send_keys(product_title + Keys.ENTER)
         url_now2 = driver.current_url
-        price = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="jm-heading-xxs jm-mb-xxs"]')))
-        price2 = price[0].text
+        price = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="a-price-whole"]')))
+        price_now2 = price[1].text
         
-    return render_template('front3.html', url_now1=url_now1, price1=price1, url_now2=url_now2, price2=price2)
+    elif 'amazon' in url:
+        title = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="a-size-large product-title-word-break"]')))
+        for titles in title:
+            product_title = titles.text
+            
+        driver.get("https://www.flipkart.com")
+        input_element = driver.find_element(By.XPATH, '//*[@class="Pke_EE"]')
+        input_element.send_keys(product_title + Keys.ENTER)
+        url_now2 = driver.current_url
+        price = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="Nx9bqj _4b5DiR"]')))
+        price_now2 = price[2].text
         
+    return render_template('front3.html', url_now1=url_now1, price1=price_now1, url_now2=url_now2, price2=price_now2)
 
 @app.route('/hello_world', methods=['POST'])
 def hello_world():
     global product1
+    global product2
     product1 = request.form['select1']
     product2 = request.form['select2']
     requirements = request.form['requirements']
     
     if 'ebay' in product1 or 'snapdeal' in product1:
         raw1 = soup(product1)
+    elif 'ebay' in product2 or 'snapdeal' in product2:
         raw2 = soup(product2)
     else:
         raw1, price1 = selenium_app(product1)
         raw2, price2 = selenium_app(product2)
     
-    test = "Could you simplify the following data and break into portions such as Name, Description, Specificaions and so on (avoid bold text): \n" + "".join(raw1)   
+    test = "Could you simplify the following data and break into portions such as Name, Description, Specificaions and so on (no bold text): \n" + "".join(raw1)   
     info1 = get_completion(test)
  
-    test = "Could you simplify the following data and break into portions such as Name, Description, Specificaions and so on (avoid bold text): \n" + "".join(raw2)
+    test = "Could you simplify the following data and break into portions such as Name, Description, Specificaions and so on (no bold text): \n" + "".join(raw2)
     info2 = get_completion(test)
     
-    test = "Comparing the two products, can you briefly describe which could be better in regards to price, performance and other details? Price for 1 = " + price1 + " Price for 2 = " + price2 + " (Speak in third person while giving bullet points please)"
+    test = "Comparing the two products, can you briefly describe which could be better in regards to " + requirements + "? (Use these only if mentioned:Price for 1 = " + price1 + " Price for 2 = " + price2 + ") (Speak in third person while avoiding bold text)"
     conclusion = get_completion(test)
     
     atexit.register(close_driver)
